e52847f8256887c1e7b9f12402ebdc2d7917a94a
wmz786 <waismalikzada9@gmail.com>
2024-08-12 15:29:17 +0430

    add styling

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -27,7 +27,7 @@ const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
       <div className="absolute -z-10 inset-0">
         <img
           src={imgSrc}
-          className="object-cover"
+          className="object-cover min-w-full min-h-full"
           alt="overlaying phone image"
         />
       </div>
