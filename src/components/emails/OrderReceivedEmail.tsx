fab5531bc380f97cefb0264f8001c65c8c855051
wmz786 <waismalikzada9@gmail.com>
2024-09-02 10:07:31 +0430

    style changes

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -68,8 +68,8 @@ const OrderReceivedEmail = ({
                 <Text style={global.paragraphWithBold}>Order Number</Text>
                 <Text style={track.number}>{orderId}</Text>
               </Column>
-              <Column>
-                <Text style={global.paragraphWithBold}>Order Number</Text>
+              <Column style={{ marginLeft: 20 }}>
+                <Text style={global.paragraphWithBold}>Order Date</Text>
                 <Text style={track.number}>{orderDate}</Text>
               </Column>
             </Row>
